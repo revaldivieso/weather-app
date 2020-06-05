ac60693eb84f2d2dbe56ea1f2dbc649999746bef
Vianny Reyes <reyesvi92@gmail.com>
2020-06-05 02:01:09 -0400

    Updates

--- Keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  WEATHER_KEY: '51da12a55be9c90c22dc1a931a9e50a7',
-};
---
+++ precache-manifest.31e711dfc605d0a22baf61cefa19aadd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bc7a5b0afa76b2f5f16f5139456587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99adf4eac309c8f2eaf0",
+    "url": "/static/css/2.408ac0c7.chunk.css"
+  },
+  {
+    "revision": "6092c50e2818d6efad84",
+    "url": "/static/css/main.a28d82f8.chunk.css"
+  },
+  {
+    "revision": "99adf4eac309c8f2eaf0",
+    "url": "/static/js/2.3349df0f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.3349df0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6092c50e2818d6efad84",
+    "url": "/static/js/main.c1de3922.chunk.js"
+  },
+  {
+    "revision": "9f1e78d60d8b52bd3d90",
+    "url": "/static/js/runtime-main.a86148e1.js"
+  }
+]);
